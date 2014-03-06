fa56ebd15a100637dee62d953cbf9be83ac5dba8
rchughye <rchughye@uwaterloo.ca>
2014-03-06 17:19:35 -0500

    Create PEuler2.py

---
+++ PEuler2.py
@@ -0,0 +1,32 @@
+# Euler Problem #2: Even Fibonacci numbers
+# http://projecteuler.net/problem=2
+# Q: By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+# A: 4613732
+
+# There is a pattern of odd and even values in the sequence
+# o, e, o, o, e, o, o, e ...
+# starting from the second number, every 3rd value afterwards is even.
+# so 2, 5, 8, 11 ... -> 2 + 3n are even for n = 0, 1 ,2 ...
+
+# Attempt via 'brute force': iterate through all Fibonacci numbers < 4 million then sum every 2 + 3n element
+
+# initialize
+
+F_old = 1
+F_new = 2
+temp = 1
+F_element = 2
+F_sum = 2
+
+# iterate
+
+while F_new <= 4000000:
+    #update the current and preivous terms. add to element counter
+    temp = F_new
+    F_new += F_old
+    F_old = temp
+    F_element += 1
+    # Add value if even
+    if F_element % 3 == 2:
+        F_sum += F_new
+print F_sum  
